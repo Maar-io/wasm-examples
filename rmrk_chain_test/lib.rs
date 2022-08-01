590a104f6b5f58514884db4d4ce1ab4466d1aa0c
Mar.io <mario@stake.co.jp>
2022-08-01 11:20:14 +0200

    added minting

--- rmrk_chain_test/lib.rs
+++ rmrk_chain_test/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use ink_env::Environment;
+use ink_env::{AccountId, Environment};
 use ink_lang as ink;
 use ink_prelude::vec::Vec;
 
@@ -12,14 +12,22 @@ use scale::{Decode, Encode};
 pub trait RmrkExtension {
     type ErrorCode = RmrkErrorCode;
 
-    #[ink(extension = 3501, returns_result = false)]
-    fn next_nft_id() -> u32;
+    #[ink(extension = 3501)]
+    fn next_nft_id(collection_id: u32) -> Result<u32, RmrkError>;
 
-    #[ink(extension = 3502, returns_result = false)]
-    fn collection_index() -> u32;
+    #[ink(extension = 3502)]
+    fn collection_index() -> Result<u32, RmrkError>;
 
     #[ink(extension = 3513)]
-    fn mint_nft(collection_id: u32, nft_id: u32, resource_id: u32) -> Result<u32, RmrkError>;
+    fn mint_ntf(
+        beneficiary: AccountId,
+        collection_id: u32,
+        royalty_recipient: Option<AccountId>,
+        royalty: Option<u8>,
+        metadata: Vec<u8>,
+        transferable: bool,
+        resources: Option< ((Vec<u8>, Vec<u8>), u32) >,
+    ) -> Result<(), RmrkError>;
 
     #[ink(extension = 3515)]
     fn create_collection(
@@ -96,10 +104,42 @@ mod rmrk_chain_test {
         }
 
         #[ink(message)]
+        pub fn next_nft_id(&self, collection_id: u32) -> Result<u32, RmrkError> {
+            let nft_id = self.env().extension().next_nft_id(collection_id)?;
+            ink_env::debug_println!("collection_index: {:?}", nft_id);
+            Ok(nft_id)
+        }
+
+        #[ink(message)]
         pub fn collection_index(&self) -> Result<u32, RmrkError> {
             let collection_id = self.env().extension().collection_index()?;
             ink_env::debug_println!("collection_index: {:?}", collection_id);
             Ok(collection_id)
+        }
+
+        #[ink(message)]
+        pub fn mint_ntf(&mut self,
+            beneficiary: AccountId,
+            collection_id: u32,
+            royalty_recipient: Option<AccountId>,
+            _royalty: Option<u8>,
+            metadata: Vec<u8>,
+            transferable: bool,
+            resources: Option< ((Vec<u8>, Vec<u8>), u32) >,
+        ) -> Result<(), RmrkError>{
+            let _result = self
+            .env()
+            .extension()
+            .mint_ntf(
+                beneficiary,
+                collection_id,
+                royalty_recipient,
+                None, // fix to use Permill
+                metadata,
+                transferable,
+                resources,
+            )?;
+            Ok(())
         }
 
         #[ink(message)]
